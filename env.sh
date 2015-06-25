a56ddc1f9d09d74935d3c20b27353779730e28b4
Kevin Pettijohn <pettijohn.k@gmail.com>
2015-06-24 21:45:46 -0700

    * Allow tags to be passed. * Pass configuration values using ENVs. * Attempt to break into smaller functions.

---
+++ env.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+export INFLUX_HOST="127.0.0.1"
+export INFLUX_PORT=8086
+export INFLUX_DATABASE="test"
+export INFLUX_USER="root"
+export INFLUX_PASSWORD="test"
+export INFLUX_BATCHSIZE=5
+export INFLUX_RP="default"
--- src/cmd/sink_influxdb/sink_influxdb.go
+++ src/cmd/sink_influxdb/sink_influxdb.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/influxdb/influxdb/client"
+	"github.com/kelseyhightower/envconfig"
 	"log"
 	"net/url"
 	"os"
@@ -13,35 +14,55 @@ import (
 	"time"
 )
 
-const (
-	Host      = "influxdb"
-	Port      = 8086
-	Database  = "test"
-	BatchSize = 20
-)
+// Configuration struct for envconfig
+type Config struct {
+	Host      string
+	Port      int
+	Database  string
+	User      string
+	Password  string
+	Batchsize int64
+	RP        string
+}
 
+// Message format from stdin.
+//
+// KeyName:key:tag1:tag_value:tag2:tag_value|vale|timestamp
+// KeyName:cpu:node_type:worker:instance_id:i-52fdg34|30|2015-06-24T05:48:56.865215158Z
 type Message struct {
 	Key   string
 	Value float64
+	Tags  map[string]string
+	Time  time.Time
 }
 
 func main() {
 
-	u, err := url.Parse(fmt.Sprintf("http://%s:%d", Host, Port))
+	// Get ENV configuration
+	var cfg Config
+	err := envconfig.Process("influx", &cfg)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	// Create influxdb connection
+	u, err := url.Parse(fmt.Sprintf("http://%s:%d", cfg.Host, cfg.Port))
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	conf := client.Config{
 		URL:      *u,
-		Username: os.Getenv("INFLUX_USER"),
-		Password: os.Getenv("INFLUX_PWD"),
+		Username: cfg.User,
+		Password: cfg.Password,
 	}
+
 	con, err := client.NewClient(conf)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// Create a scanner on stdin
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Split(bufio.ScanLines)
 	batch := make([]Message, 0)
@@ -54,75 +75,112 @@ func main() {
 			log.Fatal(err)
 		}
 
-		line := fmt.Sprint(scanner.Text())
+		// Grab a line (string) from stdin
+		line := scanner.Text()
+		// Make sure the line containes a message
 		if len(line) == 0 {
 			log.Println("String with no length. Skipping.")
 			continue
 		}
 
-		if i%BatchSize == 0 || len(line) == 0 {
-			writePoints(con, batch)
+		// If we hit our batch size write the batch of
+		// points/messages to influxdb and start the next batch.
+		if i%cfg.Batchsize == 0 {
+			pts := CreatePoints(batch)
+			WritePoints(con, cfg, pts)
 			batch = nil
 		}
 
-		line = strings.TrimSpace(line)
-		line_split := strings.Split(line, "|")
-
-		if len(line_split) < 2 {
-			log.Println("No value. Skipping.")
-			log.Println(line_split)
-			continue
-		}
-
-		key := line_split[0]
-		value, err := strconv.ParseFloat(line_split[1], 32)
-
-		if err != nil {
-			log.Println("Unable to parse float32")
-			continue
-		}
-
-		message := Message{
-			Key:   key,
-			Value: value,
-		}
-
-		if err != nil {
-			log.Println("Unable to read string from stdin")
-		}
-
+		message := CreateMessage(line)
 		batch = append(batch, message)
 
-		log.Printf("Key: %s Value: %g", message.Key, message.Value)
+		// log.Printf("Key: %s Value: %g", message.Key, message.Value)
 	}
-	writePoints(con, batch)
+
+	if len(batch) == 0 {
+		log.Println("Message with no length. Skipping write and exiting..")
+		os.Exit(0)
+	}
+	// Write any remaining points/messages to influxdb
+	pts := CreatePoints(batch)
+	WritePoints(con, cfg, pts)
 }
 
-func writePoints(con *client.Client, batch []Message) {
-	pts := make([]client.Point, 0)
-	region := "us-west-1b"
-	node_type := "api"
+func CreateMessage(line string) Message {
+	// Remove any leading and trailing white space.
+	// Including \n
+	line = strings.TrimSpace(line)
+	line_split := strings.Split(line, "|")
 
+	if len(line_split) < 2 {
+		log.Println("No value. Skipping.")
+	}
+
+	tags := TagSplit(line_split[0])
+
+	key := tags["KeyName"]
+	delete(tags, "KeyName")
+	value, err := strconv.ParseFloat(line_split[1], 32)
+
+	if err != nil {
+		log.Println("Unable to parse float32")
+	}
+
+	const RFC3339 = "2006-01-02T15:04:05Z07:00"
+	timestamp, err := time.Parse(RFC3339, line_split[2])
+	if err != nil {
+		log.Println("Unable to parse time")
+	}
+
+	message := Message{
+		Key:   key,
+		Value: value,
+		Tags:  tags,
+		Time:  timestamp,
+	}
+
+	return message
+}
+
+func TagSplit(t string) map[string]string {
+	tagSlice := strings.Split(t, ":")
+	tags := make(map[string]string)
+
+	for i, _ := range tagSlice {
+		if i%2 == 0 {
+			v := i + 1
+			if v < len(tagSlice) {
+				tags[tagSlice[i]] = tagSlice[v]
+			} else {
+				continue
+			}
+		}
+	}
+	return tags
+}
+
+func CreatePoints(batch []Message) []client.Point {
+	pts := make([]client.Point, 0)
 	for _, m := range batch {
 		pt := client.Point{
 			Measurement: m.Key,
-			Tags: map[string]string{
-				"region":    region,
-				"node_type": node_type,
-			},
+			Tags:        m.Tags,
 			Fields: map[string]interface{}{
 				"value": m.Value,
 			},
-			Time:      time.Now(),
+			Time:      m.Time,
 			Precision: "s",
 		}
 		pts = append(pts, pt)
 	}
+	return pts
+}
 
+func WritePoints(con *client.Client, cfg Config, pts []client.Point) {
 	bps := client.BatchPoints{
 		Points:          pts,
-		Database:        Database,
-		RetentionPolicy: "default",
+		Database:        cfg.Database,
+		RetentionPolicy: cfg.RP,
 	}
 
 	message_debug, _ := json.Marshal(bps)
